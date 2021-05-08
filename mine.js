cd18dd8d46c78b368edcf13a9adbb9d289347fb4
HodaRoushdy <83883044+HodaRoushdy@users.noreply.github.com>
2021-05-08 22:28:10 +0200

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,165 @@
+/*var x = window.prompt("enter a number :");
+console.log(x);*/
+/*var x = window.prompt("enter a number :");
+if(x %3 == 0 && x %4 == 0 ){
+    console.log("yes");
+}
+else{
+    console.log("no");
+}*/
+/*var x = window.prompt("enter first number :");
+var y = window.prompt("enter second number :");
+if(x>y){
+    console.log(x);
+}
+else if(y>x){
+  console.log(y);
+}*/
+/*var x = window.prompt("enter number :");
+if(x>0){
+    console.log("positive");
+}
+else if(x<0){
+    console.log("negative");
+}*/
+/*
+var x = window.prompt("enter first number :");
+var y = window.prompt("enter second number :");
+var z = window.prompt("enter third number :");
+if(x > y && x > z){
+    console.log("x is maximum");
+}
+else if(y>x && y>z){
+    console.log("y is maximum");
+}
+else if(z>x && z>y){
+    console.log("z is maximum");
+}
+if(x < y && x < z){
+    console.log("x is minmum");
+}
+else if(y<x && y<z){
+    console.log("y is minmum");
+}
+else if(z<x && z<y){
+    console.log("z is minmum");
+}*/ /*
+var x = window.prompt("enter number :");
+if(x%2 == 0){
+    console.log("x is even ");
+}
+else{
+    console.log("x is odd");
+}*/  /*
+var x = window.prompt("enter character :");
+if(x =='a' || x=='e'||x=='i'|| x=='o' || x =='u'){
+    console.log("x is vowel");
+}
+else{
+    console.log("x is costant");
+}*/ /*
+var x = window.prompt("enter intger :");
+for(var i =1;i <= x;i++){
+    console.log(i);
+}*/
+/*
+var x = window.prompt("enter intger :");
+for(var i =1;i <= 12;i++){
+    console.log(i*x);
+}*/  /*
+var x = window.prompt("enter intger :");
+for(var i =1;i <= x;i++){
+    if(i%2==0)
+    console.log(i);
+}*/ /*
+var x = window.prompt("enter first number :");
+var y = window.prompt("enter second number :");
+for(var i =1;i <= y;i++){
+    s=x*x;
+    s=x*s;
+}
+console.log(s); */  /*
+var x = Number (window.prompt("enter first number :"));
+var y = Number(window.prompt("enter second number :"));
+var z = Number(window.prompt("enter third number :"));
+var s = Number(window.prompt("enter forth number :"));
+var w = Number(window.prompt("enter fifth number :"));
+ var T=x+y+z+s+w;
+var v=T/5;
+var p=(T/500)*100;
+console.log("Total ="+ T);
+console.log("Average ="+ v);
+console.log("percentage ="+ p); */  /*
+var x = window.prompt("enter number :");
+switch(x){
+ case 1:
+ console.log("Days in month : 31");
+ break;
+ case 2:
+ console.log("Days in month : 29");
+ break;
+ case 3:
+ console.log("Days in month : 31");
+ break;
+ case 4:
+ console.log("Days in month : 30");
+ break;
+ case 5:
+ console.log("Days in month : 31");
+ break;
+ case 6:
+ console.log("Days in month : 30");
+ break;
+ case 7:
+ console.log("Days in month : 31");
+ break;
+ case 8:
+ console.log("Days in month : 31");
+ break;
+ case 9:
+ console.log("Days in month : 30");
+ break;
+ case 10:
+ console.log("Days in month : 31");
+ break;
+ case 11:
+ console.log("Days in month : 30");
+ break;
+ case 12:
+ console.log("Days in month : 31"); 
+ default:
+ console.log("enter number between 1 to 12 only"); 
+}*/   /*
+var x = window.prompt("enter number :");
+if(x == 1 || x==3 || x==5 || x==7 || x==8 ||x==10 || x==12 )
+{
+    console.log("Days in month : 31");
+}
+else if(x==4 || x== 6 || x== 9|| x==11){
+    console.log("Days in month : 30");
+}
+else if(x==2){
+    console.log("Days in month : 29");
+}
+else{
+    console.log("enter number between 1 to 12 only"); 
+}*/    /*
+var p = Number(window.prompt("enter Physics marks"));
+var ch = Number(window.prompt("enter , Chemistry marks"));
+var b = Number(window.prompt("enter Biology marks"));
+var m = Number(window.prompt("enter Mathematics marks"));
+var c = Number(window.prompt("enter Computer marks"));
+ var t = p+ch+b+m+c;
+ per = (t/500)*100;
+ if(per >= 90)
+  console.log("grad = A");
+  else if(per >= 80)
+  console.log("grad = B");
+  else if(per >= 70)
+  console.log("grad = C");
+  else if(per >= 60)
+  console.log("grad = D");
+  else if(per >= 40)
+  console.log("grad = E");
+  else if(per < 40)
+  console.log("grad = F");*/
